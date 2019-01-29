1e34ee5d20e6e8c9dc02cae8892a097238d748e8
Josh Freeman <jdfreeman11@gmail.com>
2019-01-29 14:10:54 -0600

    Remove placeholder test

--- lib/perspective.ex
+++ lib/perspective.ex
@@ -1,18 +1,2 @@
 defmodule Perspective do
-  @moduledoc """
-  Documentation for Perspective.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Perspective.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/perspective_test.exs
+++ test/perspective_test.exs
@@ -1,8 +1,2 @@
 defmodule PerspectiveTest do
-  use ExUnit.Case
-  doctest Perspective
-
-  test "greets the world" do
-    assert Perspective.hello() == :world
-  end
 end
