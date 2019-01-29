e2dc2e3226c23fe851272c1174fe347fb1a09c93
Josh Freeman <jdfreeman11@gmail.com>
2019-01-29 14:34:55 -0600

    Reduce version number to base 0.0.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Perspective.MixProject do
     [
       app: :perspective,
       name: "perspective",
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/backmath/perspective",
