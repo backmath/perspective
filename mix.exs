f3fa9210dde527ec7c7da109dd78d6a34b194c50
Josh Freeman <jdfreeman11@gmail.com>
2019-01-29 14:28:58 -0600

    Update mix.exs, add LICENSE.md

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule Perspective.MixProject do
   def project do
     [
       app: :perspective,
+      name: "perspective",
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: "https://github.com/backmath/perspective",
+      description: description(),
+      package: package(),
+      deps: deps(),
     ]
   end
 
@@ -24,6 +28,19 @@ defmodule Perspective.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    "An event sourcing library based upon asynchronously generated perspectives"
+  end
+
+  defp package do
+    [
+      maintainers: ["Josh Freeman"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/backmath/perspective"},
+      files: ~w(lib CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
     ]
   end
 end
