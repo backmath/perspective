756910634feffda16ceb7311ff5ca56f0008f46e
Josh Freeman <jdfreeman11@gmail.com>
2019-02-06 10:33:29 -0600

    Install uuid

--- mix.exs
+++ mix.exs
@@ -26,8 +26,7 @@ defmodule Perspective.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:uuid, "~> 1.1"}
     ]
   end
 
