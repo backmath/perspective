26ba5d912f8d9022eb29421882c21ff8d28f9e2f
Josh Freeman <jdfreeman11@gmail.com>
2019-02-19 11:52:25 -0600

    Update Notifications to include id'd structs

--- lib/perspective/notifications/notifications.ex
+++ lib/perspective/notifications/notifications.ex
@@ -1,9 +1,21 @@
 defmodule Perspective.Notifications do
-  def subscribe(%event_name{}) do
-    Phoenix.PubSub.subscribe(__MODULE__, to_string(event_name))
+  def subscribe(event) do
+    Phoenix.PubSub.subscribe(__MODULE__, topic_name(event))
   end
 
-  def emit(%event_name{} = event) do
-    Phoenix.PubSub.broadcast!(__MODULE__, to_string(event_name), event)
+  def subscribe(event, id) do
+    Phoenix.PubSub.subscribe(__MODULE__, topic_name(event, id))
   end
+
+  def emit(event) do
+    Phoenix.PubSub.broadcast!(__MODULE__, topic_name(event), event)
+  end
+
+  def emit(event, id) do
+    Phoenix.PubSub.broadcast!(__MODULE__, topic_name(event, id), event)
+  end
+
+  defp topic_name(%event_name{}), do: to_string(event_name)
+  defp topic_name(%event_name{}, nil = _id), do: to_string(event_name)
+  defp topic_name(%event_name{}, id), do: "#{to_string(event_name)}.#{id}"
 end
--- lib/perspective/notifications/test/notifications_test.exs
+++ lib/perspective/notifications/test/notifications_test.exs
@@ -5,10 +5,18 @@ defmodule Perspective.Notifications.Test do
     defstruct [:id]
   end
 
-  test "a process can subscribe to structs being emited" do
+  test "a process can subscribe to structs being emitted" do
     Perspective.Notifications.subscribe(%ExampleEvent{})
 
     Perspective.Notifications.emit(%ExampleEvent{id: :hello})
+
+    assert_receive %ExampleEvent{id: :hello}
+  end
+
+  test "a process can subscribe to structs and ids being emitted" do
+    Perspective.Notifications.subscribe(%ExampleEvent{}, "todo:abc-123")
+
+    Perspective.Notifications.emit(%ExampleEvent{id: :hello}, "todo:abc-123")
 
     assert_receive %ExampleEvent{id: :hello}
   end
