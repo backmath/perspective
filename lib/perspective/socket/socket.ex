a99cd7edc133b5420c6c150212f31b74c63f3bc2
Josh Freeman <jdfreeman11@gmail.com>
2019-02-20 14:06:53 -0600

    Introduce Perspective.Socket

---
+++ lib/perspective/socket/socket.ex
@@ -0,0 +1,26 @@
+defmodule Perspective.Socket do
+  defmacro __using__(_) do
+    quote do
+      use Phoenix.Socket
+      import Perspective.Socket
+    end
+  end
+
+  defmacro projection(module, opts \\ []) do
+    {path, channel, _reactor} = Macro.expand(module, __ENV__).projection
+
+    quote do
+      channel(unquote(path), unquote(channel), unquote(opts))
+    end
+  end
+
+  defp tear_alias({:__aliases__, meta, [h | t]}) do
+    alias = {:__aliases__, meta, [h]}
+
+    quote do
+      Module.concat([unquote(alias) | unquote(t)])
+    end
+  end
+
+  defp tear_alias(other), do: other
+end
