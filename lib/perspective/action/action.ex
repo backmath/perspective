c718d351cc717fcd40a7379b30b9c7ff22f7efde
Josh Freeman <jdfreeman11@gmail.com>
2019-03-09 16:05:12 -0600

    Attach metadata when transforming an action

--- lib/perspective/action/action.ex
+++ lib/perspective/action/action.ex
@@ -5,16 +5,22 @@ defmodule Perspective.Action do
     calling_module = __CALLER__.module()
 
     quote do
-      def transform(%unquote(calling_module){} = unquote(agent)), do: unquote(block)
+      def transform(%unquote(calling_module){} = unquote(agent)), do: attach_metadata(unquote(block))
 
       defimpl Perspective.ActionTransformer, for: unquote(calling_module) do
-        def transform(%unquote(calling_module){} = unquote(agent)), do: unquote(block)
+        def transform(%unquote(calling_module){} = unquote(agent)), do: attach_metadata(unquote(block))
       end
 
       def transform(%wrong_type{} = action) do
         raise(WrongActionType, {action, unquote(calling_module)})
       end
     end
+  end
+
+  def attach_metadata(event) do
+    event
+    |> Map.put(:id, UUID.uuid4())
+    |> Map.put(:date, DateTime.utc_now() |> to_string())
   end
 
   defmacro __using__(_opts) do
--- lib/perspective/domain_event/domain_event.ex
+++ lib/perspective/domain_event/domain_event.ex
@@ -1,9 +1,19 @@
 defmodule Perspective.DomainEvent do
   defmacro __using__(_options) do
     quote do
+      import Kernel, except: [defstruct: 1]
       import Perspective.DomainEvent
       use Perspective.ModuleRegistry
       register_module(Perspective.DomainEvent)
+    end
+  end
+
+  defmacro defstruct(args) do
+    # todo: throw an error if args contians an event, date key
+    arguments = [:event_id | [:date | args]]
+
+    quote do
+      Kernel.defstruct(unquote(arguments))
     end
   end
 end
