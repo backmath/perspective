bdd00b463d46f54f560e2b10147cd5065d529f7b
Josh Freeman <jdfreeman11@gmail.com>
2019-03-10 17:21:51 -0500

    Remove deprecated callbacks

--- lib/perspective/authentication/guardian.ex
+++ lib/perspective/authentication/guardian.ex
@@ -6,15 +6,7 @@ defmodule Perspective.Guardian do
     {:ok, sub}
   end
 
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
-  end
-
   def resource_from_claims(claims) do
     {:ok, claims["sub"]}
-  end
-
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
   end
 end
