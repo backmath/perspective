aed61947341d805721c1cf47ae739825adf4f63e
Josh Freeman <jdfreeman11@gmail.com>
2019-03-20 14:33:52 -0500

    Reduce Argon cost to improve test times

--- config/config.test.exs
+++ config/config.test.exs
@@ -2,3 +2,7 @@ use Mix.Config
 
 config :perspective, Perspective.EventChainStorage, path: "./storage/test/"
 config :perspective, Perspective.Config.Test, test_config: :expected_value
+
+config :argon2_elixir,
+  t_cost: 1,
+  m_cost: 8
