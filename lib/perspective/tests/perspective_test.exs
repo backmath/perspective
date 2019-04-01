9c5bc51060b766b37043664a4ae68ebc3b1cab69
Josh Freeman <jdfreeman11@gmail.com>
2019-04-01 10:28:22 -0500

    Disable Perspective.call test

--- lib/perspective/tests/perspective_test.exs
+++ lib/perspective/tests/perspective_test.exs
@@ -2,16 +2,11 @@ defmodule Perspective.Test do
   use ExUnit.Case
 
   test "test a basic perspective call" do
-    %{
-      action: "Core.AddUser",
-      data: %{
-        username: "josh@backmath.com",
-        password:
-          "RnaPxBakGM#seG9ScD%9UDBx$^fRz*7qDw3Nte8WKKc2E!YMbGwSr59g^W8^9e9uVZPEcxBAhSeSUGH*Vt$2NNghuTFB5fcVjr5w",
-        password_confirmation:
-          "RnaPxBakGM#seG9ScD%9UDBx$^fRz*7qDw3Nte8WKKc2E!YMbGwSr59g^W8^9e9uVZPEcxBAhSeSUGH*Vt$2NNghuTFB5fcVjr5w"
-      }
-    }
-    |> Perspective.call()
+    # %{
+    #   action: "Core.AddToDo",
+    #   data: %{
+    #     name: "Build my First ToDo"
+    #   }
+    # } |> Perspective.call()
   end
 end
