5122851b438213093550c817a4195e7aac24b619
Josh Freeman <jdfreeman11@gmail.com>
2019-04-03 17:53:22 -0500

    Simplify processor and make procedures explicit

--- lib/perspective/processor/processor.ex
+++ lib/perspective/processor/processor.ex
@@ -1,30 +1,36 @@
 defmodule Perspective.Processor do
   def run(request) do
-    # authorize request (not authenticate)
-
-    # Validate syntax
-    case valid_action?(request.action) do
-      true -> complete_request(request)
-      false -> validation_errors(request.action)
-    end
-  end
-
-  defp valid_action?(%action_type{} = action) do
-    action_type.valid?(action)
-  end
-
-  defp validation_errors(%action_type{} = action) do
-    action_type.errors?(action)
-  end
-
-  defp complete_request(request) do
     request
+    |> validate_syntax()
+    |> authorize_request()
+    |> validate_semantics()
     |> transform_request_to_event()
     |> apply_to_the_event_chain()
   end
 
+  defp validate_syntax(request) do
+    case Perspective.ActionRequest.SyntaxValidator.validate_syntax(request) do
+      [] -> request
+      errors -> raise "Something with Errors"
+    end
+  end
+
+  defp authorize_request(request) do
+    case Perspective.ActionRequest.RequestAuthorizer.authorize_request(request) do
+      [] -> request
+      errors -> raise "Something with Errors"
+    end
+  end
+
+  defp validate_semantics(request) do
+    case Perspective.ActionRequest.SemanticValidator.validate_semantics(request) do
+      [] -> request
+      errors -> raise "Something with Errors"
+    end
+  end
+
   defp transform_request_to_event(request) do
-    Perspective.ActionTransformer.transform(request.action)
+    Perspective.DomainEvent.RequestTransformer.to_event(request)
   end
 
   defp apply_to_the_event_chain(domain_event) do
--- lib/perspective/processor/tests/processor_test.exs
+++ lib/perspective/processor/tests/processor_test.exs
@@ -2,7 +2,10 @@ defmodule Perspective.Processor.Test do
   use ExUnit.Case
 
   test "example run" do
-    assert {:ok, _action} = Perspective.Processor.run(action_request())
+    Core.AddToDo.new(%{
+      name: "Test Perspective.Processor.run"
+    })
+    |> Perspective.Processor.run()
 
     result = Perspective.EventChain.last()
 
