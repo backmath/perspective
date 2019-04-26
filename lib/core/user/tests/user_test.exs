08fbd54a50312fec521dd1e319c462243711f316
Josh Freeman <jdfreeman11@gmail.com>
2019-04-26 14:31:26 -0500

    Remove deprecated user functionality tests

--- lib/core/user/tests/user_test.exs
+++ lib/core/user/tests/user_test.exs
@@ -1,23 +1,3 @@
 defmodule Core.User.Test do
   use ExUnit.Case
-
-  test "apply_event: AddUser " do
-    user = %Core.User{}
-
-    event = %Core.UserAdded{
-      data: %{
-        user_id: "user:abc-123",
-        username: "josh@backmath.com"
-      }
-    }
-
-    expected = %Core.User{
-      id: "user:abc-123",
-      username: "josh@backmath.com"
-    }
-
-    assert {:ok, result} = Core.User.apply_event(user, event)
-
-    assert result == expected
-  end
 end
--- lib/core/user/user.ex
+++ lib/core/user/user.ex
@@ -2,13 +2,4 @@ defmodule Core.User do
   use Perspective.DomainNode
 
   defstruct id: "", username: ""
-
-  def apply_event(_todo, %Core.UserAdded{} = event) do
-    %Core.User{}
-    |> Map.put(:id, event.data.user_id)
-    |> Map.put(:username, event.data.username)
-    |> case do
-      new_todo -> {:ok, new_todo}
-    end
-  end
 end
