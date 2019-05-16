ed524f93f4be4f9489377a75361d43f8daacb5a9
Josh Freeman <jdfreeman11@gmail.com>
2019-05-16 15:44:33 -0500

    Update Perspective.Config to use different modules

--- lib/perspective/config/config.ex
+++ lib/perspective/config/config.ex
@@ -1,10 +1,18 @@
 defmodule Perspective.Config do
-  defmacro __using__(_) do
-    calling_module = __CALLER__.module
+  defmacro __using__(key) do
+    # raise an error with wrong usage
+    optional_key =
+      case Macro.expand(key, __CALLER__) do
+        nil -> nil
+        [] -> nil
+        atom when is_atom(atom) -> atom
+      end
+
+    config_key = optional_key || __CALLER__.module
 
     quote do
       def config(key, default \\ nil) do
-        Application.get_env(:perspective, unquote(calling_module))
+        Application.get_env(:perspective, unquote(config_key))
         |> Keyword.get(key, default)
       end
     end
--- lib/perspective/config/tests/config_test.exs
+++ lib/perspective/config/tests/config_test.exs
@@ -2,7 +2,27 @@ defmodule Perspective.Config.Test do
   use ExUnit.Case
   use Perspective.Config
 
+  defmodule Example do
+    use Perspective.Config
+
+    def a, do: config(:test_config)
+    def b, do: config(:absent_config, :another_value)
+  end
+
+  defmodule ExampleOverride do
+    use Perspective.Config, Perspective.Config.Test.SpecialOverride
+
+    def a, do: config(:test_config)
+    def b, do: config(:absent_config, :another_value)
+  end
+
   test "use Perspective.Config gives access to application configuration" do
-    assert :expected_value == config(:test_config)
+    assert :expected_value == Example.a()
+    assert :another_value == Example.b()
+  end
+
+  test "use Perspective.Config can use different module names" do
+    assert :expected_value == ExampleOverride.a()
+    assert :another_value == ExampleOverride.b()
   end
 end
