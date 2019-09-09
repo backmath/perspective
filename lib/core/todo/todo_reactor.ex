b0e9b9cd4ff354e3c7ec82df0b2abbc1e9d45e6f
Josh Freeman <jdfreeman11@gmail.com>
2019-09-09 16:49:29 -0500

    Add creator_id to created ToDos

--- lib/core/todo/todo_reactor.ex
+++ lib/core/todo/todo_reactor.ex
@@ -1,10 +1,11 @@
 defmodule Core.ToDo.Reactor do
   use Perspective.Reactor
 
-  update(%Core.ToDoAdded{data: data}, _state) do
+  update(%Core.ToDoAdded{data: data, actor_id: actor_id}, _state) do
     %Core.ToDo{}
     |> Map.put(:id, data.todo_id)
     |> Map.put(:name, data.name)
+    |> Map.put(:creator_id, actor_id)
     |> Perspective.DomainPool.put!()
   end
 
