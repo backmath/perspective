15ce83690e5adf1e0f7e06f509146503d1bd3cd0
Josh Freeman <jdfreeman11@gmail.com>
2019-09-16 03:00:22 -0500

    Remove Deprecated SystemTime Projection

--- lib/core/system/projections/system_time.ex
+++
@@ -1,5 +0,0 @@
-defmodule Core.Projections.SystemTime do
-  use Perspective.Projection
-
-  expose("system://time", Core.Services.SystemClock)
-end
--- lib/core/system/projections/tests/system_time_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Core.System.SystemTime.Test do
-  use ExUnit.Case
-
-  test "receiving an event will generate the expected result" do
-    # How to test?
-  end
-end
