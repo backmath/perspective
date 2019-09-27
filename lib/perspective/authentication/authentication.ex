18319602610a21d66d93df9506057e6c48d8947d
Josh Freeman <jdfreeman11@gmail.com>
2019-09-27 15:35:19 -0500

    Add @todo

--- lib/perspective/authentication/authentication.ex
+++ lib/perspective/authentication/authentication.ex
@@ -2,6 +2,7 @@ defmodule Perspective.Authentication do
   use Perspective.Config
 
   def authenticate_request(request, token) do
+    # @todo: throw if misconfigured
     authentication_module = config(:module)
 
     authentication_module.authenticate_request(request, token)
