0a292d7e8e4a9396c836c94f517ed3a8339f6b13
Josh Freeman <jdfreeman11@gmail.com>
2019-09-27 15:17:37 -0500

    Add RequestGenerator.from for stringed keys

--- lib/perspective/request/request_generator.ex
+++ lib/perspective/request/request_generator.ex
@@ -7,6 +7,18 @@ defmodule Perspective.RequestGenerator do
     end
   end
 
+  def from(%{"data" => data, "action" => action}) do
+    try do
+      create_action(action, data)
+    rescue
+      error in Perspective.Request.MissingAction -> {:error, error}
+    end
+  end
+
+  def from(abc) do
+    raise "The provided request had no matcher: #{abc}"
+  end
+
   defp create_action(action_name, data) do
     try do
       struct_name = String.to_existing_atom("Elixir.#{action_name}")
