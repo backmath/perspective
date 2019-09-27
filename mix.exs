22bbda848696729c93dee207c01c8ea95eccc5df
Josh Freeman <jdfreeman11@gmail.com>
2019-09-27 14:05:31 -0500

    Release version 0.1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Perspective.MixProject do
     [
       app: :perspective,
       name: "perspective",
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       source_url: "https://github.com/backmath/perspective",
