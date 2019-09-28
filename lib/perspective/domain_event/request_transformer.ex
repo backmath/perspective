f5369b7219fdfd5e07f2eb8571f7c466768fd1ce
Josh Freeman <jdfreeman11@gmail.com>
2019-09-28 12:56:14 -0500

    Add private DDD names to RequestTransformer

--- lib/perspective/domain_event/request_transformer.ex
+++ lib/perspective/domain_event/request_transformer.ex
@@ -1,10 +1,35 @@
 defmodule Perspective.DomainEvent.RequestTransformer do
-  def to_event(%action_request{} = request) do
+  def to_event(request) do
+    request
+    |> initialize_event
+    |> transform_id_from_request_to_event
+    |> set_the_event_date
+    |> set_the_domain_event_version(request)
+    |> transform_the_data(request)
+    |> transform_the_metadata(request)
+  end
+
+  defp initialize_event(%action_request{} = request) do
     struct(action_request.domain_event_name(), Map.from_struct(request))
-    |> Map.put(:id, String.replace(request.id, ~r/request/, "event"))
-    |> Map.put(:event_date, DateTime.utc_now())
-    |> Map.put(:version, action_request.domain_event_version())
-    |> Map.put(:data, Perspective.ActionRequest.RequestDataTransformer.transform_data(request))
-    |> Map.put(:meta, Perspective.ActionRequest.MetadataTransformer.transform_meta(request))
+  end
+
+  defp transform_id_from_request_to_event(event) do
+    Map.put(event, :id, String.replace(event.id, ~r/request/, "event"))
+  end
+
+  defp set_the_event_date(event) do
+    Map.put(event, :event_date, DateTime.utc_now())
+  end
+
+  defp set_the_domain_event_version(event, %action_request{}) do
+    Map.put(event, :version, action_request.domain_event_version())
+  end
+
+  defp transform_the_data(event, request) do
+    Map.put(event, :data, Perspective.ActionRequest.RequestDataTransformer.transform_data(request))
+  end
+
+  defp transform_the_metadata(event, request) do
+    Map.put(event, :meta, Perspective.ActionRequest.MetadataTransformer.transform_meta(request))
   end
 end
