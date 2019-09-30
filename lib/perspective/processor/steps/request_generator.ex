40abdfc058a255fd8bced9e9fd70e28402a607a4
Josh Freeman <jdfreeman11@gmail.com>
2019-09-30 00:22:31 -0500

    Atomize struct data when generating requests

--- lib/perspective/processor/steps/request_generator.ex
+++ lib/perspective/processor/steps/request_generator.ex
@@ -14,7 +14,7 @@ defmodule Perspective.Processor.RequestGenerator do
   defp create_action(action_name, data) do
     try do
       struct_name = String.to_existing_atom("Elixir.#{action_name}")
-      struct_name.new(nil, data)
+      struct_name.new(nil, Perspective.AtomizeKeys.atomize_keys(data))
     rescue
       _error in ArgumentError -> raise Perspective.Request.MissingAction, action_name
     end
