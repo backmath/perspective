97da46d11afab18600ce4867a9315e7f428e8d59
Josh Freeman <jdfreeman11@gmail.com>
2019-10-10 16:30:30 -0500

    Improve NodeNotFound message

--- lib/perspective/node_pool/errors/node_not_found.ex
+++ lib/perspective/node_pool/errors/node_not_found.ex
@@ -1,4 +1,6 @@
 defmodule Perspective.NodePool.NodeNotFound do
+  import Perspective.StripElixir
+
   defexception [:id, :node_pool]
 
   def exception({id, node_pool}) do
@@ -9,6 +11,6 @@ defmodule Perspective.NodePool.NodeNotFound do
   end
 
   def message(%__MODULE__{id: id, node_pool: node_pool}) do
-    "The domain node (#{id}) could not be found in the pool (#{node_pool})"
+    "The domain node (#{id}) could not be found in the pool (#{strip_elixir(node_pool)})"
   end
 end
--- lib/perspective/node_pool/errors/tests/node_not_found_test.exs
+++ lib/perspective/node_pool/errors/tests/node_not_found_test.exs
@@ -2,7 +2,7 @@ defmodule Perspective.Core.DomainPool.NodeNotFound.Test do
   use ExUnit.Case, async: true
 
   test "raising a NodeNotFound error provides a helpful message" do
-    expected_message = "The domain node (abc-123) could not be found in the pool (Elixir.SomeNodePool)"
+    expected_message = "The domain node (abc-123) could not be found in the pool (SomeNodePool)"
 
     assert_raise(Perspective.NodePool.NodeNotFound, expected_message, fn ->
       raise Perspective.NodePool.NodeNotFound, {"abc-123", SomeNodePool}
