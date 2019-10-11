05f7b2968154a71d20b5ddcc11b8e88bad171de7
Josh Freeman <jdfreeman11@gmail.com>
2019-10-10 21:33:27 -0500

    Enable Perspective.call with action structs

--- lib/perspective/processor/steps/request_generator.ex
+++ lib/perspective/processor/steps/request_generator.ex
@@ -1,4 +1,10 @@
 defmodule Perspective.Processor.RequestGenerator do
+  import Perspective.StripElixir
+
+  def generate(%action{data: data}) do
+    create_action(action, data)
+  end
+
   def generate(%{data: data, action: action}) do
     create_action(action, data)
   end
@@ -13,7 +19,7 @@ defmodule Perspective.Processor.RequestGenerator do
 
   defp create_action(action_name, data) do
     try do
-      struct_name = String.to_existing_atom("Elixir.#{action_name}")
+      struct_name = String.to_existing_atom("Elixir.#{strip_elixir(action_name)}")
       struct_name.new(nil, Perspective.AtomizeKeys.atomize_keys(data))
     rescue
       _error in ArgumentError -> raise Perspective.Request.MissingAction, action_name
--- lib/perspective/processor/steps/tests/request_generator_test.exs
+++ lib/perspective/processor/steps/tests/request_generator_test.exs
@@ -1,10 +1,20 @@
 defmodule Perspective.Processor.RequestGenerator.Test do
   use ExUnit.Case, async: true
 
-  test "generating a request from a struct" do
+  test "generating a request from a map" do
     result =
       %{
         action: "Perspective.Core.AddToDo",
+        data: %{}
+      }
+      |> Perspective.Processor.RequestGenerator.generate()
+
+    assert %Perspective.Core.AddToDo{} = result
+  end
+
+  test "generating a request from a struct" do
+    result =
+      %Perspective.Core.AddToDo{
         data: %{}
       }
       |> Perspective.Processor.RequestGenerator.generate()
