e680d2666d61f90e3843f0b76c5ae00576c84ca3
Josh Freeman <jdfreeman11@gmail.com>
2019-11-14 15:47:28 -0600

    Ensure true, :ok, nil, and [] pass validation

--- lib/perspective/processor/steps/semantic_validator.ex
+++ lib/perspective/processor/steps/semantic_validator.ex
@@ -1,6 +1,9 @@
 defmodule Perspective.Processor.SemanticValidator do
   def validate(request) do
     case Perspective.ActionRequest.SemanticValidator.validate_semantics(request) do
+      true -> request
+      :ok -> request
+      nil -> request
       [] -> request
       errors -> raise(Perspective.InvalidSemantics, errors: errors, request: request)
     end
--- lib/perspective/processor/steps/syntax_validator.ex
+++ lib/perspective/processor/steps/syntax_validator.ex
@@ -1,6 +1,9 @@
 defmodule Perspective.Processor.SyntaxValidator do
   def validate(request) do
     case Perspective.ActionRequest.SyntaxValidator.validate_syntax(request) do
+      true -> request
+      :ok -> request
+      nil -> request
       [] -> request
       errors -> raise(Perspective.InvalidSyntax, errors: errors, request: request)
     end
