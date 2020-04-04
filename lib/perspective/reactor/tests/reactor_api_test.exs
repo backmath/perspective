85fcd2db97fb6251d289f321778f6a787ce99b1d
Josh Freeman <jdfreeman11@gmail.com>
2020-04-03 22:15:03 -0500

    Clarify Perspective.Reactor tests

--- lib/perspective/reactor/tests/reactor_api_test.exs
+++ lib/perspective/reactor/tests/reactor_api_test.exs
@@ -21,15 +21,19 @@ defmodule Perspective.Reactor.API.Test do
     use Perspective.Reactor
   end
 
-  test "initial_state generated from a macro" do
+  test "initial_state macro generates module function correctly" do
     assert [:a] == Perspective.Reactor.API.Test.Example.initial_state()
   end
 
-  test "update generated from a macro" do
+  test "update macro generates correctly and yields expected value" do
     assert [:b, :a] == Perspective.Reactor.API.Test.Example.update(%ExampleEvent{}, [:a])
   end
 
-  test "default initial_state is nil" do
+  test "subscribed_events returns the event  yields expected value" do
+    assert [Perspective.Reactor.API.Test.ExampleEvent] = Perspective.Reactor.API.Test.Example.updateable_events()
+  end
+
+  test "a reactors default initial state is nil" do
     assert nil == Perspective.Reactor.API.Test.DefaultReactor.initial_state()
   end
 
@@ -37,9 +41,5 @@ defmodule Perspective.Reactor.API.Test do
     assert_raise Perspective.Reactor.UnsupportedEvent, fn ->
       Perspective.Reactor.API.Test.Example.update(:unsupported, [:a])
     end
-  end
-
-  test "subscribed_events generated from a macro" do
-    assert [Perspective.Reactor.API.Test.ExampleEvent] = Perspective.Reactor.API.Test.Example.updateable_events()
   end
 end
