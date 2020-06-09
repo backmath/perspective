d76ba5f9156df2ffb49c2f84f8e6788cd5feee4d
Josh Freeman <jdfreeman11@gmail.com>
2020-06-08 21:48:28 -0500

    Standardize AppID lookup

--- lib/perspective/utilities/app_id/app_id_lookup.ex
+++ lib/perspective/utilities/app_id/app_id_lookup.ex
@@ -1,4 +1,4 @@
-defmodule Perspective.AppID do
+defmodule Perspective.AppIDLookup do
   def fetch_and_set do
     case fetch_from_process_dictionary() do
       nil -> lookup_from_ancestors_and_set()
