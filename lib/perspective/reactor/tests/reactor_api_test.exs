cac468dd0151895e75b9ed1e2f8bb60d0fa3c629
Josh Freeman <jdfreeman11@gmail.com>
2020-07-06 09:44:16 -0500

    Clarify the Reactor API test descriptions

--- lib/perspective/reactor/tests/reactor_api_test.exs
+++ lib/perspective/reactor/tests/reactor_api_test.exs
@@ -1,43 +1,43 @@
 defmodule Perspective.Reactor.API.Test do
   use Perspective.TestCase
 
-  defmodule ExampleEvent do
-    defstruct data: :b
+  defmodule DefaultReactor do
+    use Perspective.Reactor
   end
 
   defmodule Example do
     use Perspective.Reactor
 
+    defmodule Event do
+      defstruct data: :b
+    end
+
     initial_state do
       [:a]
     end
 
-    update(%ExampleEvent{} = event, state) do
+    update(%Example.Event{} = event, state) do
       [event.data | state]
     end
   end
 
-  defmodule DefaultReactor do
-    use Perspective.Reactor
-  end
-
-  test "calling initial_state macro generates a module function with the same body" do
-    assert [:a] == Perspective.Reactor.API.Test.Example.initial_state()
-  end
-
-  test "calling update macro generates a module function with the same body" do
-    assert [:b, :a] == Perspective.Reactor.API.Test.Example.update(%ExampleEvent{}, [:a])
-  end
-
-  test "subscribed_events returns the event  yields expected value" do
-    assert [Perspective.Reactor.API.Test.ExampleEvent] = Perspective.Reactor.API.Test.Example.updateable_events()
-  end
-
-  test "a reactors default initial state is nil" do
+  test "a reactor's default initial state is nil" do
     assert nil == Perspective.Reactor.API.Test.DefaultReactor.initial_state()
   end
 
-  test "run_update throws an error for unsupported events" do
+  test "a reactor's initial state can be defined by calling the initial_state macro" do
+    assert [:a] == Perspective.Reactor.API.Test.Example.initial_state()
+  end
+
+  test "a reactor transforms received events and state as defined by calling the update macro" do
+    assert [:b, :a] == Perspective.Reactor.API.Test.Example.update(%Example.Event{}, [:a])
+  end
+
+  test "subscribed_events returns all events registered through Reactor.update" do
+    assert [Perspective.Reactor.API.Test.Example.Event] = Perspective.Reactor.API.Test.Example.updateable_events()
+  end
+
+  test "sending an event not supported throws an error (should not happen in practice)" do
     assert_raise Perspective.Reactor.UnsupportedEvent, fn ->
       Perspective.Reactor.API.Test.Example.update(:unsupported, [:a])
     end
